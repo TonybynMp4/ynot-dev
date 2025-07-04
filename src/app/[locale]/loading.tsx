8dba6a5f9cce65b24ba7216588a9590f2b8eb898
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-07-04 10:49:29 +0200

    feat: kibo ui spinner

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,10 +1,9 @@
-import { LoaderIcon } from "lucide-react";
+import { Spinner } from "@/components/ui/kibo-ui/spinner";
 
 export default function Loading() {
 	return (
 		<main className="flex items-center justify-center h-screen">
-			<LoaderIcon className="h-12 w-12 animate-spin animation-duration-1750" />
-			<p className="ml-4 text-2xl text-muted-foreground">Chargement...</p>
+			<Spinner variant="bars" className="size-24" />
 		</main>
 	);
 }
